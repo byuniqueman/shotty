dcdfa27f36ab581a654975fd169cf8e7372a7467
Brandon Yu <me@brandonyu.com>
2019-10-18 00:32:42 -0700

    added instance details

--- shotty-ec2-instances.py
+++ shotty-ec2-instances.py
@@ -1,9 +1,30 @@
 #!/usr/local/bin/python3
 
 import boto3
-if __name__ == '__main__':
-    session = boto3.Session(profile_name='shotty')
-    ec2 = session.resource('ec2')
-    
+import click
+
+session = boto3.Session(profile_name='shotty')
+ec2 = session.resource('ec2')
+
+@click.command() # hand off function to click
+def list_instances():
+    "List EC2 instances" # docstring
     for i in ec2.instances.all():
-        print(i)
+        print(', '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name)))
+            
+    # instance attributes
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html#instance
+    
+    return
+    
+if __name__ == '__main__':
+    list_instances()
+    
+    
+    
+
